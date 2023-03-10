4b6f5b11b1399cb8094470d9c31488b5baf35f83
Chris Rybicki <chrisr@monada.co>
2023-03-09 23:08:19 -0500

    commit wasm files

---
+++ pkg/index.d.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {any} files
+* @param {any} diagnostic
+* @param {any} config
+* @returns {string}
+*/
+export function emit(files: any, diagnostic: any, config: any): string;
